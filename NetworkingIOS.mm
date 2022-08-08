e1ba799eb5d30e17317d134d1190fd5105fc9b73
Stephen Quan <squan@esri.com>
2022-08-08 16:28:21 +1000

    Update NetworkingIOS.mm
    Remove debugging code.

--- NetworkingIOS.mm
+++ NetworkingIOS.mm
@@ -1,7 +1,6 @@
 #include "NetworkingIOS.h"
 #import <Foundation/Foundation.h>
 
-#include <QDebug>
 #include <QUrl>
 
 #include <CoreServices/CoreServices.h>
@@ -12,10 +11,9 @@ QNetworkProxy NetworkingIOS::systemProxy()
 {
     NSDictionary* proxySettings =  CFBridgingRelease(CFNetworkCopySystemProxySettings());
     bool isHTTPEnable = [proxySettings[(__bridge NSString*)kCFNetworkProxiesHTTPEnable] boolValue];
-    qDebug() << Q_FUNC_INFO << "isHTTPEnable: " << isHTTPEnable;
     if (!isHTTPEnable)
     {
-        //return QNetworkProxy();
+        return QNetworkProxy();
     }
 
     NSString* nsHttpProxy =  proxySettings[(__bridge NSString*)kCFNetworkProxiesHTTPProxy];
@@ -24,11 +22,9 @@ QNetworkProxy NetworkingIOS::systemProxy()
         return QNetworkProxy();
     }
     QString httpProxy = QString::fromNSString(nsHttpProxy);
-    qDebug() << Q_FUNC_INFO << "HttpProxy: " << httpProxy;
 
     NSUInteger nsHttpPort = [proxySettings[(__bridge NSString*)kCFNetworkProxiesHTTPPort] unsignedIntegerValue];
     int httpPort = static_cast<int>(nsHttpPort);
-    qDebug() << Q_FUNC_INFO << "HttpPort: " << httpPort;
-
+ 
     return QNetworkProxy(QNetworkProxy::HttpProxy, httpProxy, httpPort);
 }
